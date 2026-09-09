42409829338ab1c23e49d7ecd3c2c6c96ea3ca58
Alejandro <ramirezaed@gmail.com>
2026-09-09 15:19:27 -0300

    feat: agrega controlador registrar usuario

--- app/Http/Controllers/api/v1/AuthController.php
+++ app/Http/Controllers/api/v1/AuthController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\api\v1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Auth\registerUserFormRequest;
+use App\Http\Resources\Auth\registerUserResource;
+use App\Services\Auth\registerUserService;
+use Illuminate\Http\JsonResponse;
 
 class AuthController extends Controller
 {
     //
+    public function __construct(private registerUserService $register_user_service) {}
+
+    public function register(registerUserFormRequest $request): JsonResponse
+    {
+        $user = $this->register_user_service->create($request->toDTO());
+        return response()->json(new registerUserResource($user), 201);
+    }
 }
