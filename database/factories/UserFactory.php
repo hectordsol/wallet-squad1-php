1d194745b85f0f9517812802b07413b1f2518ddd
JulioAndres2021 <94810568+JulioAndres2021@users.noreply.github.com>
2026-09-10 09:34:30 -0300

    Revert "WAL-005 y WAL-006 - Perfil y cuenta propios"

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,13 +4,18 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 /**
  * @extends Factory<User>
  */
 class UserFactory extends Factory
 {
-    protected $model = User::class;
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
 
     /**
      * Define the model's default state.
@@ -20,12 +25,21 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'nombre' => fake()->name(),
+            'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'password' => 'password',
-            'edad' => fake()->numberBetween(18, 80),
-            'imagen' => null,
-            'rol' => 'usuario',
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
         ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
     }
 }
